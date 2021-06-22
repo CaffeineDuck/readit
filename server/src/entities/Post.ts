1bfa35e01a93c42b3a15d8c8cc362ddaf5022530
Samrid Pandit <samrid.pandit@gmail.com>
2021-06-22 16:57:20 +0545

    Created a post entity

---
+++ server/src/entities/Post.ts
@@ -0,0 +1,17 @@
+import { PrimaryKey, Property } from "@mikro-orm/core";
+import { Entity } from "@mikro-orm/core";
+
+@Entity()
+export class Post {
+    @PrimaryKey()
+    id!: number
+
+    @Property()
+    createdAt = new Date()
+
+    @Property({onUpdate: () => new Date()})
+    updatedAt = new Date()
+
+    @Property()
+    title!: string
+}
