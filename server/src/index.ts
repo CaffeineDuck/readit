b3f0cab6581079ab44f27dc31ae747d7c8ace59f
Samrid Pandit <samrid.pandit@gmail.com>
2021-06-22 16:57:57 +0545

    Initialized mikro orm

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello there')
+import { MikroORM } from "@mikro-orm/core";
+import { Post } from "./entities/Post";
+import { __prod__ } from "./utils/constants";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    dbName: "readit_db",
+    user: "test",
+    password: "test",
+    debug: !__prod__,
+    type: "postgresql",
+    entities: [Post],
+  });
+
+ };
+
+main();
